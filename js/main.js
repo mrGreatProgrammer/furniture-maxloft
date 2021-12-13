ade7f67f4b2a84cc870533fe7e2c52ca759ef9e1
Student <fhgf9048@gmail.com>
2021-12-13 16:36:46 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const loaderWelcome = document.querySelector('.loader-welcome');
+
+const theLoader = document.querySelector(".the-loader");
+const theLoaderProgress = document.querySelector(".the-loader-progress");
+
+const theSite = document.querySelector('.the-site');
+
+const toTopUpEl = document.querySelector("#to-top-up")
+
+var i =0 
+var inID = setInterval(() => {
+i += 50
+
+  theLoaderProgress.style.width = i + "px";
+}, 500);
+
+
+setTimeout(() => {
+    clearInterval(inID)
+    console.log("h");
+    loaderWelcome.style.display = "none";
+    setTimeout(() => {
+        theSite.style.display = "block"
+    }, 500);
+    
+}, 5500);
+
+
+
+window.onscroll = () => {
+  if (window.scrollY > 700) {
+    toTopUpEl.classList.remove('isShowBtn_hide')
+  } else if (window.scrollY < 700) {
+    toTopUpEl.classList.add('isShowBtn_hide')
+  }
+};
+
+toTopUpEl.onclick = () => {
+  window.scrollTo(0, 0)
+}
