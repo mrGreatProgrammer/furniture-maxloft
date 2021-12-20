1d148e55cd26b7fcb41ec833a48754ed377956fc
Student <fhgf9048@gmail.com>
2021-12-20 12:19:25 +0500

    formated code

--- js/main.js
+++ js/main.js
@@ -1,41 +1,38 @@
-const loaderWelcome = document.querySelector('.loader-welcome');
+const loaderWelcome = document.querySelector(".loader-welcome");
 
 const theLoader = document.querySelector(".the-loader");
 const theLoaderProgress = document.querySelector(".the-loader-progress");
 
-const theSite = document.querySelector('.the-site');
+const theSite = document.querySelector(".the-site");
 
-const toTopUpEl = document.querySelector("#to-top-up")
+const toTopUpEl = document.querySelector("#to-top-up");
 
 // LOADER
-var i =0 
+var i = 0;
 var inID = setInterval(() => {
-i += 50
+  i += 50;
 
   theLoaderProgress.style.width = i + "px";
 }, 500);
 
-
 setTimeout(() => {
-    clearInterval(inID)
-    console.log("h");
-    loaderWelcome.style.display = "none";
-    setTimeout(() => {
-        theSite.style.display = "block"
-    }, 500);
-    
+  clearInterval(inID);
+  console.log("h");
+  loaderWelcome.style.display = "none";
+  setTimeout(() => {
+    theSite.style.display = "block";
+  }, 500);
 }, 5500);
-
 
 // SCROLLING
 window.onscroll = () => {
   if (window.scrollY > 700) {
-    toTopUpEl.classList.remove('isShowBtn_hide')
+    toTopUpEl.classList.remove("isShowBtn_hide");
   } else if (window.scrollY < 700) {
-    toTopUpEl.classList.add('isShowBtn_hide')
+    toTopUpEl.classList.add("isShowBtn_hide");
   }
 };
 
 toTopUpEl.onclick = () => {
-  window.scrollTo(0, 0)
-}
+  window.scrollTo(0, 0);
+};
